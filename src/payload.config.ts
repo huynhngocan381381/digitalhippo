5fcbab84224a1a3dc36509ebbf3e37b771af7673
Huynh Ngoc An <huynhngocan381381@gmail.com>
2024-01-14 13:19:02 +0700

    Setting up admin dashboard

---
+++ src/payload.config.ts
@@ -0,0 +1,36 @@
+import { buildConfig } from 'payload/config'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import path from 'path'
+import dotenv from 'dotenv'
+import { viteBundler } from '@payloadcms/bundler-vite'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+
+dotenv.config({
+  path: path.resolve(__dirname, '../.env'),
+})
+
+export default buildConfig({
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
+  collections: [], 
+  routes: {
+    admin: '/sell',
+  },
+  admin: {
+    bundler: viteBundler(),
+    meta: {
+      titleSuffix: '- DigitalHippo',
+      favicon: '/favicon.ico',
+      ogImage: '/thumbnail.jpg',
+    },
+  },
+  rateLimit: {
+    max: 2000,
+  },
+  editor: lexicalEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URL!,
+  }),
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+})
