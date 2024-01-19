8bc9f4eb2a8a241281178e2002311970e8b015cd
Huynh Ngoc An <huynhngocan381381@gmail.com>
2024-01-19 23:49:21 +0700

    Proper error handling & finishing auth

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -1,7 +1,7 @@
-import { TRPCError } from '@trpc/server'
-import { getPayloadClient } from '../get-payload'
 import { AuthCredentialsValidator } from '../lib/validators/account-credentials-validator'
 import { publicProcedure, router } from './trpc'
+import { getPayloadClient } from '../get-payload'
+import { TRPCError } from '@trpc/server'
 import { z } from 'zod'
 
 export const authRouter = router({
@@ -46,5 +46,27 @@ export const authRouter = router({
     if (!isVerified) throw new TRPCError({ code: 'UNAUTHORIZED' })
 
     return { success: true }
+  }),
+
+  signIn: publicProcedure.input(AuthCredentialsValidator).mutation(async ({ input, ctx }) => {
+    const { email, password } = input
+    const { res } = ctx
+
+    const payload = await getPayloadClient()
+
+    try {
+      await payload.login({
+        collection: 'users',
+        data: {
+          email,
+          password,
+        },
+        res,
+      })
+
+      return { success: true }
+    } catch (err) {
+      throw new TRPCError({ code: 'UNAUTHORIZED' })
+    }
   }),
 })
