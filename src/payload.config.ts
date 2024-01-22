9e10630d0fc409c0e65473b489ec24865b2253b7
Huynh Ngoc An <huynhngocan381381@gmail.com>
2024-01-22 12:14:05 +0700

    Modelling data & enabling uploads

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,6 +5,10 @@ import dotenv from 'dotenv'
 import { viteBundler } from '@payloadcms/bundler-vite'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { Users } from './collections/Users'
+import { Products } from './collections/Products/Products'
+import { Media } from './collections/Media'
+import { ProductFiles } from './collections/ProductFile'
+import { Orders } from './collections/Orders'
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
@@ -12,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users], 
+  collections: [Users, Products, Media, ProductFiles, Orders], 
   routes: {
     admin: '/sell',
   },
