373ae5d8b59d24ca5a29b660a308ec83785f85f6
Huynh Ngoc An <huynhngocan381381@gmail.com>
2024-02-04 23:35:20 +0700

    Creating the thank-you page

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,9 +1,9 @@
-import { TRPCError } from '@trpc/server'
-import type Stripe from 'stripe'
 import { z } from 'zod'
+import { privateProcedure, publicProcedure, router } from './trpc'
+import { TRPCError } from '@trpc/server'
 import { getPayloadClient } from '../get-payload'
 import { stripe } from '../lib/stripe'
-import { privateProcedure, router } from './trpc'
+import type Stripe from 'stripe'
 
 export const paymentRouter = router({
   createSession: privateProcedure
@@ -48,7 +48,7 @@ export const paymentRouter = router({
       })
 
       line_items.push({
-        price: 'price_1OfP51JqRUsAGfSEgxILITe8',
+        price: 'price_1OCeBwA19umTXGu8s4p2G3aX',
         quantity: 1,
         adjustable_quantity: {
           enabled: false,
@@ -59,7 +59,7 @@ export const paymentRouter = router({
         const stripeSession = await stripe.checkout.sessions.create({
           success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
           cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
-          payment_method_types: ['card'],
+          payment_method_types: ['card', 'paypal'],
           mode: 'payment',
           metadata: {
             userId: user.id,
@@ -73,4 +73,26 @@ export const paymentRouter = router({
         return { url: null }
       }
     }),
+  pollOrderStatus: privateProcedure.input(z.object({ orderId: z.string() })).query(async ({ input }) => {
+    const { orderId } = input
+
+    const payload = await getPayloadClient()
+
+    const { docs: orders } = await payload.find({
+      collection: 'orders',
+      where: {
+        id: {
+          equals: orderId,
+        },
+      },
+    })
+
+    if (!orders.length) {
+      throw new TRPCError({ code: 'NOT_FOUND' })
+    }
+
+    const [order] = orders
+
+    return { isPaid: order._isPaid }
+  }),
 })
